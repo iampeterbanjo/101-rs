9c50105a4d5de5bde51b37a65748af743a0c46fe
iampeterbanjo <me@iampeterbanjo.com>
2023-09-07 11:04:33 +0100

    feat(A1): complete move semantics

--- exercises/A1/2-move-semantics/src/bin/03.rs
+++ exercises/A1/2-move-semantics/src/bin/03.rs
@@ -1,9 +1,9 @@
 //! Make me compile without adding new lines-- just changing existing lines!
 
 fn main() {
-    let s0 = String::new();
+    let mut s0 = String::new();
 
-    let mut s1 = append_to_string(s0);
+    let s1 = append_to_string(&mut s0);
 
     println!("{} == `{}`", stringify!(s1), s1);
 
@@ -12,7 +12,7 @@ fn main() {
     println!("{} == `{}`", stringify!(s1), s1);
 }
 
-fn append_to_string(s: String) -> String {
+fn append_to_string(s: &mut String) -> &mut String {
     s.push_str("Hello World");
 
     s
